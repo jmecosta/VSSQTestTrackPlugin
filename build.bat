378479d1053db3fcba22a9bae13ecfad70d2b000
Jorge Costa <Jorge.Costa@tekla.com>
2015-11-19 12:53:12 +0200

    dont skip copy

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo on
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat"
-msbuild BuildPlugins.msbuild /p:VisualStudioVersion=14.0 /v:diag /p:EndVSQFile=VSSQTestTrackPlugin.VSQ /p:AssemblyPatcherTaskOn=true > buildlog2015.txt
+msbuild BuildPlugins.msbuild /p:VisualStudioVersion=14.0 /v:diag /p:EndVSQFile=VSSQTestTrackPlugin.VSQ /p:AssemblyPatcherTaskOn=true /p:SkipCopy=No > buildlog2015.txt
